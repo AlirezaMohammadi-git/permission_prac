0ecce2dcb7380c8ed23fc636437f0b498b83ea13
alireza02899 <109007288+alireza02899@users.noreply.github.com>
2022-09-18 06:59:20 +0430

    everything works well except bluetooth for android 12 and above

--- app/src/main/java/com/example/yaghotpractic/MainActivity.kt
+++ app/src/main/java/com/example/yaghotpractic/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.yaghotpractic
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.TaskStackBuilder.create
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Intent
@@ -15,17 +16,39 @@ import com.example.yaghotpractic.databinding.ActivityMainBinding
 import android.provider.Settings
 
 import android.content.Context
+import android.content.IntentSender
 import android.content.pm.PackageManager
+import android.hardware.HardwareBuffer.create
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationRequest
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
+import android.net.Uri
 import android.widget.Button
+import androidx.appcompat.app.AppCompatDelegate.create
+import androidx.appcompat.graphics.drawable.AnimatedStateListDrawableCompat.create
 import androidx.core.app.ActivityCompat
-
+import androidx.core.widget.EdgeEffectCompat.create
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.LocationSettingsStatusCodes
+import com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY
+import com.google.android.material.color.HarmonizedColorAttributes.create
+import java.io.IOException
+import java.net.URI.create
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var wifiManager: WifiManager
     lateinit var myBlueTooth: BluetoothAdapter
+    lateinit var locationRequest: com.google.android.gms.location.LocationRequest
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +56,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         myBlueTooth = BluetoothAdapter.getDefaultAdapter()
         setContentView(binding.root)
+
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
         myBlueTooth = BluetoothAdapter.getDefaultAdapter()
         val bluetoothManager =
@@ -63,6 +89,7 @@ class MainActivity : AppCompatActivity() {
             enableBluetooth.setOnClickListener {
                 var intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
                 startActivity(intent)
+                //todo = blotooth dosn't work for 12 and 13 android
             }
             disableBluetooth.setOnClickListener {
                 if (myBlueTooth.isEnabled) {
@@ -74,6 +101,12 @@ class MainActivity : AppCompatActivity() {
                     toast(this@MainActivity, "Bluetooth is already off")
                 }
             }
+            binding.getLocation.setOnClickListener {
+                checkLocationPermission()
+                checkGPS()
+                getUserLocation()
+            }
+
         }
     }
 
@@ -101,6 +134,63 @@ class MainActivity : AppCompatActivity() {
         }
 
     private fun checkGPS() {
-        TODO("Not yet implemented")
+        locationRequest = com.google.android.gms.location.LocationRequest.create()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            locationRequest.priority = LocationRequest.QUALITY_HIGH_ACCURACY
+        }else{
+            locationRequest.priority = com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+        locationRequest.interval = 5000
+            locationRequest.fastestInterval = 2000
+
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+            builder.setAlwaysShow(true)
+
+        getUserLocation()
+
+        val result = LocationServices.getSettingsClient(this.applicationContext).checkLocationSettings(builder.build())
+
+        result.addOnCompleteListener { task ->
+            try {
+                //when the gps id on
+                val response = task.getResult(ApiException::class.java)
+            }catch ( e : ApiException )
+            {
+                //when the gps is off
+                e.printStackTrace()
+                when (e.statusCode){
+                    LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> try {
+                    //here we send request to enable gps
+                        val resoApiException = e as ResolvableApiException
+                        resoApiException.startResolutionForResult(this,200)
+                    }catch (sendIntendExeption : IntentSender.SendIntentException){
+                            //when settings are not available
+                    }
+                }
+            }
+        }
     }
+
+    @SuppressLint("MissingPermission")
+    private fun getUserLocation() {
+        fusedLocationProviderClient.lastLocation.addOnCompleteListener{ task ->
+            val location = task.getResult()
+            if (location != null)
+            {
+                try {
+                val geocoder = Geocoder(this , Locale.getDefault())
+                    val address = geocoder.getFromLocation( location.latitude , location.latitude , 1 )
+                    //here we set the address to text view
+                    val address_Line = address[0].getAddressLine(0)
+                    binding.textView.text = address_Line.toString()
+                }catch (e : IOException)
+                {
+
+                }
+            }
+        }
+
+    }
+
 }
